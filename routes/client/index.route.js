ef44b2d5b6cb1cdb961a38599561536cab6ff078
Hung <hung.nt225195@sis.hust.edu.vn>
2025-04-19 22:29:35 +0700

    '??'

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,6 @@ const productsRoute = require("./product.route");
 const homeRoute = require("./home.route");
 module.exports = (app) => {
     app.use("/", homeRoute);
-
+    
     app.use("/products", productsRoute);
 }
